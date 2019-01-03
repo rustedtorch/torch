3187c33854be4ef366dec3a0d45d60d0d30c669d
Adrian Punga <adrian.punga@ejobsgroup.ro>
2019-01-03 22:55:49 +0200

    Implemented all macros for all supported number types

--- src/lib.rs
+++ src/lib.rs
@@ -3,66 +3,98 @@ extern crate num_traits;
 pub mod function;
 pub mod tensor;
 
-pub fn to_value(x: &f64) -> Result<&f64, &'static str> {
-    Ok(x)
-}
+// let flattened_data = vec![];
+// let dimensions = vec![];
+// tensor::Tensor::new(flattened_data, dimensions);
 
 #[macro_export(local_inner_macros)]
 macro_rules! tensor {
     ($($element:tt)+) => {
-        tensor_internal!($($element)+)
+        tensor_f32!($($element)+)
+    };
+}
+
+#[macro_export(local_inner_macros)]
+macro_rules! tensor_f64 {
+    ($($element:tt)+) => {
+        tensor_internal!(f64, $($element)+)
+    };
+}
+
+#[macro_export(local_inner_macros)]
+macro_rules! tensor_f32 {
+    ($($element:tt)+) => {
+        tensor_internal!(f32, $($element)+)
+    };
+}
+
+#[macro_export(local_inner_macros)]
+macro_rules! tensor_i64 {
+    ($($element:tt)+) => {
+        tensor_internal!(i64, $($element)+)
+    };
+}
+
+#[macro_export(local_inner_macros)]
+macro_rules! tensor_i32 {
+    ($($element:tt)+) => {
+        tensor_internal!(i32, $($element)+)
+    };
+}
+
+#[macro_export(local_inner_macros)]
+macro_rules! tensor_i16 {
+    ($($element:tt)+) => {
+        tensor_internal!(i16, $($element)+)
     };
 }
 
 #[macro_export(local_inner_macros)]
 macro_rules! tensor_internal {
     // Done with trailing comma.
-    (@array [$($elems:expr,)*]) => {
-        tensor_internal_vec![$($elems,)*]
+    ($type:ty, @array [$($elems:expr,)*]) => {
+       tensor_internal_vec![$($elems,)*]
     };
 
     // Done without trailing comma.
-    (@array [$($elems:expr),*]) => {
+    ($type:ty, @array [$($elems:expr),*]) => {
         tensor_internal_vec![$($elems),*]
     };
 
     // Next element is an array.
-    (@array [$($elems:expr,)*] [$($array:tt)*] $($rest:tt)*) => {
-        tensor_internal!(@array [$($elems,)* tensor_internal!([$($array)*])] $($rest)*)
+    ($type:ty, @array [$($elems:expr,)*] [$($array:tt)*] $($rest:tt)*) => {
+        tensor_internal!($type, @array [$($elems,)* tensor_internal!($type, [$($array)*])] $($rest)*)
     };
 
     // Next element is an expression followed by comma.
-    (@array [$($elems:expr,)*] $next:expr, $($rest:tt)*) => {
-        tensor_internal!(@array [$($elems,)* tensor_internal!($next),] $($rest)*)
+    ($type:ty, @array [$($elems:expr,)*] $next:expr, $($rest:tt)*) => {
+        tensor_internal!($type, @array [$($elems,)* tensor_internal!($type, $next),] $($rest)*)
     };
 
     // Last element is an expression with no trailing comma.
-    (@array [$($elems:expr,)*] $last:expr) => {
-        tensor_internal!(@array [$($elems,)* tensor_internal!($last)])
+    ($type:ty, @array [$($elems:expr,)*] $last:expr) => {
+        tensor_internal!($type, @array [$($elems,)* tensor_internal!($type, $last)])
     };
 
     // Comma after the most recent element.
-    (@array [$($elems:expr),*] , $($rest:tt)*) => {
-        tensor_internal!(@array [$($elems,)*] $($rest)*)
+    ($type:ty, @array [$($elems:expr),*] , $($rest:tt)*) => {
+        tensor_internal!($type, @array [$($elems,)*] $($rest)*)
     };
 
     // Unexpected token after most recent element.
-    (@array [$($elems:expr),*] $unexpected:tt $($rest:tt)*) => {
+    ($type:ty, @array [$($elems:expr),*] $unexpected:tt $($rest:tt)*) => {
         tensor_unexpected!($unexpected)
     };
 
-    ([]) => {
-        tensor_internal_vec![]
-    };
-
-    ([ $($tt:tt)+ ]) => {
-        tensor_internal!(@array [] $($tt)+)
+    ($type:ty, [ $($tt:tt)+ ]) => {
+        tensor_internal!($type, @array [] $($tt)+)
     };
 
     // Any Serialize type: numbers, strings, struct literals, variables etc.
     // Must be below every other rule.
-    ($other:expr) => {
-        $crate::to_value(&$other).unwrap()
+    ($type:ty, $other:expr) => {
+        // And we force it to always be $type
+        (|val: $type| -> $type { val }) ($other)
     };
 }
 
