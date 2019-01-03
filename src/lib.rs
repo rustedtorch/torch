c6b16fdd383e47712e2aecaa54c2a1be08151b05
Adrian Punga <adrian.punga@ejobsgroup.ro>
2019-01-03 19:33:35 +0200

    Updated structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,4 @@
 extern crate num_traits;
 
-mod function;
-mod tensor;
-
-use tensor::Tensor;
-
-pub fn test() -> Result<(), &'static str> {
-    let x = Tensor::new_from_vector(vec![1.0, 2.01, 3.02, 4.5]);
-    let y = x.add(Tensor::new_from_vector(vec![0.1, 0.2, 0.3, 0.4]))?;
-    let z = Tensor::new_from_cube(vec![
-        vec![
-            vec![1.0, 2.01, 3.02, 4.5],
-            vec![1.0, 2.01, 3.02, 4.5],
-            vec![1.0, 2.01, 3.02, 4.5],
-            vec![1.0, 2.01, 3.02, 4.5],
-        ],
-        vec![
-            vec![1.0, 2.01, 3.02, 4.5],
-            vec![1.0, 2.01, 3.02, 4.5],
-            vec![1.0, 2.01, 3.02, 4.5],
-            vec![1.0, 2.01, 3.02, 4.5],
-        ],
-    ]);
-    println!("{:?}", z);
-    Ok(())
-}
+pub mod function;
+pub mod tensor;
