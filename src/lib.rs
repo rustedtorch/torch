87a3649680524e3deb7f225dea7412707e5400fb
Adrian Punga <adrian.punga@ejobsgroup.ro>
2019-01-04 00:14:11 +0200

    Finalized any-dimensional tensor literal

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,65 @@
 extern crate num_traits;
+use num_traits::Num;
 
 pub mod function;
 pub mod tensor;
 
-// let flattened_data = vec![];
-// let dimensions = vec![];
-// tensor::Tensor::new(flattened_data, dimensions);
+pub enum Data<T> {
+    Item(T),
+    Vector(Vec<Data<T>>),
+}
+
+pub fn flatten<T: 'static + Num + Clone + std::fmt::Debug>(data: Data<T>) -> tensor::Tensor<T> {
+    let (flattened_data, dimensions) = flatten_recursive(data);
+    tensor::Tensor::new(flattened_data, dimensions)
+}
+
+pub fn flatten_recursive<T>(data: Data<T>) -> (Vec<T>, Vec<usize>) {
+    match data {
+        Data::Item(item) => (vec![item], vec![]),
+        Data::Vector(vector) => {
+            let mut flattened_data = vec![];
+            let mut current_inner_dimension = 0;
+            let mut dimensions = vec![vector.len()];
+            let mut dimension_recorded = false;
+            for item in vector {
+                let (inner_flattened_data, inner_dimensions) = flatten_recursive(item);
+                if !dimension_recorded {
+                    if inner_dimensions.len() > 0 {
+                        current_inner_dimension = inner_dimensions[0];
+                    }
+                    dimensions.extend(inner_dimensions);
+                    dimension_recorded = true;
+                } else {
+                    if inner_dimensions.len() > 0 {
+                        if current_inner_dimension != inner_dimensions[0] {
+                            if current_inner_dimension == 0 {
+                                panic!(
+                                    "Expected scalar got vector of length {}",
+                                    inner_dimensions[0]
+                                );
+                            } else {
+                                panic!(
+                                    "Expected vector of length {} got vector of length {}",
+                                    current_inner_dimension, inner_dimensions[0]
+                                );
+                            }
+                        }
+                    } else {
+                        if current_inner_dimension != 0 {
+                            panic!(
+                                "Expected vector of length {} got scalar",
+                                current_inner_dimension
+                            );
+                        }
+                    }
+                }
+                flattened_data.extend(inner_flattened_data);
+            }
+            (flattened_data, dimensions)
+        }
+    }
+}
 
 #[macro_export(local_inner_macros)]
 macro_rules! tensor {
@@ -17,35 +71,35 @@ macro_rules! tensor {
 #[macro_export(local_inner_macros)]
 macro_rules! tensor_f64 {
     ($($element:tt)+) => {
-        tensor_internal!(f64, $($element)+)
+        $crate::flatten(tensor_internal!(f64, $($element)+))
     };
 }
 
 #[macro_export(local_inner_macros)]
 macro_rules! tensor_f32 {
     ($($element:tt)+) => {
-        tensor_internal!(f32, $($element)+)
+        $crate::flatten(tensor_internal!(f32, $($element)+))
     };
 }
 
 #[macro_export(local_inner_macros)]
 macro_rules! tensor_i64 {
     ($($element:tt)+) => {
-        tensor_internal!(i64, $($element)+)
+        $crate::flatten(tensor_internal!(i64, $($element)+))
     };
 }
 
 #[macro_export(local_inner_macros)]
 macro_rules! tensor_i32 {
     ($($element:tt)+) => {
-        tensor_internal!(i32, $($element)+)
+        $crate::flatten(tensor_internal!(i32, $($element)+))
     };
 }
 
 #[macro_export(local_inner_macros)]
 macro_rules! tensor_i16 {
     ($($element:tt)+) => {
-        tensor_internal!(i16, $($element)+)
+        $crate::flatten(tensor_internal!(i16, $($element)+))
     };
 }
 
@@ -94,14 +148,14 @@ macro_rules! tensor_internal {
     // Must be below every other rule.
     ($type:ty, $other:expr) => {
         // And we force it to always be $type
-        (|val: $type| -> $type { val }) ($other)
+        (|val: $type| -> $crate::Data<$type> { $crate::Data::Item(val) }) ($other)
     };
 }
 
 #[macro_export]
 macro_rules! tensor_internal_vec {
     ($($content:tt)*) => {
-        vec![$($content)*]
+        $crate::Data::Vector(vec![$($content)*])
     };
 }
 
